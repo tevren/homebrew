77e2008aa68b4468682c13d34722c52c92ec6f09
Anurag Mohanty <amohanty@amplify.com>
2014-03-31 14:38:34 -0400

    fixed where options are placed in command

--- providers/cask.rb
+++ providers/cask.rb
@@ -15,7 +15,7 @@ action :cask do
     if new_resource.options
       options = new_resource.options.join(" ")
       execute "installing cask #{new_resource.name}" do
-        command "/usr/local/bin/brew cask install #{new_resource.name} #{options}"
+        command "/usr/local/bin/brew cask install #{options} #{new_resource.name}"
         not_if "/usr/local/bin/brew brew list | grep #{new_resource.name}"
       end
     else
